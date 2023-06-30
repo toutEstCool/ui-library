585adc8d0add18e45ad7a1fd723dc4ff98c51ae6
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-06-30 13:42:22 +0600

    [PM]: slider-fn#2

--- js/index.js
+++ js/index.js
@@ -1,30 +1,22 @@
 document.querySelector('.player-chunk-prev').addEventListener('click', function () {
-  function moveClass (className, activeClassName) {
-    let 
-    active = document.querySelector('.' + activeClassName)
-    prev = active.previousElementSibling
-
-    if (prev && prev.classList.contains(className)) {
-      active.classList.remove(activeClassName)
-      prev.classList.add(activeClassName)
-    }
-  }
-  moveClass('timeline-chunk', 'timeline-chunk-active')
-  moveClass('player-chunk', 'player-chunk-active')
+  moveClass('timeline-chunk-active', 'previousElementSibling')
+  moveClass('player-chunk-active', 'previousElementSibling')
 })
 
 document.querySelector('.player-chunk-next').addEventListener('click', function () {
-  function moveClass (className) {
-    let 
-    active = document.querySelector('.' + className)
-
-    if (active.nextElementSibling) {
-      active.classList.remove(className)
-      active.nextElementSibling.classList.add(className)
-    }
-  }
-  moveClass('timeline-chunk-active')
-  moveClass('player-chunk-active')
+  moveClass('timeline-chunk-active', 'nextElementSibling')
+  moveClass('player-chunk-active', 'nextElementSibling')
 })
+
+function moveClass (className, method) {
+  let
+    active = document.querySelector(`.${className}`)
+    next = active[method]
+
+  if (next) {
+    active.classList.remove(className)
+    next.classList.add(className)
+  }
+}
 
 
